cbe597c7c245bb18f827dadd36e2ac1b00621090
dianatoti <dianatoti10@gmail.com>
2025-11-13 15:01:24 +0100

    creato array e generato numeri random per le due proprietà specificate

---
+++ snack-2/js/script.js
@@ -0,0 +1,26 @@
+// Snack2
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const squadre = [
+  { nome: "Juventus", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Milan", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Inter", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Roma", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Napoli", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Lazio", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Atalanta", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Fiorentina", puntiFatti: 0, falliSubiti: 0 }
+];
+
+const randomNumberPuntiFatti = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
+// console.log(randomNumberPuntiFatti);
+
+const randomNuberFalliSubiti = Math.floor(Math.random() * (50 - 0 + 1)) + 0;
+console.log(randomNuberFalliSubiti);
+
+
+
